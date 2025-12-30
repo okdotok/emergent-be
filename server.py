c57c6cb5f63a741388058f4074ff6600e2bd38d9
Edison Guga <guged683@gmail.com>
2025-12-31 06:34:05 +0900

    Fix deployment: Add start.sh script, use .get() for env vars, fix runtime.txt format

--- server.py
+++ server.py
@@ -28,9 +28,10 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
+db_name = os.environ.get('DB_NAME', 'theglobal_uren')
 client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+db = client[db_name]
 
 # Security
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
